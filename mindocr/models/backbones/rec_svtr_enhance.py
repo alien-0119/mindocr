f59b0da0134ec150cf0d9ce61d103e9ace7cc4f7
alien_0119 <wangtianyu43@huawei.com>
2025-07-10 18:05:55 +0800

    fix ci

--- mindocr/models/backbones/rec_svtr_enhance.py
+++ mindocr/models/backbones/rec_svtr_enhance.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This code is refer from: 
+# This code is refer from:
 # https://github.com/PaddlePaddle/PaddleClas/blob/develop/ppcls/arch/backbone/legendary_models/pp_lcnet.py
 
 
